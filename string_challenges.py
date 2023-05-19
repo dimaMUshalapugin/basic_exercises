1e9a453298bc7c804d13c569d0cdf1c98c1157e7
dimaMUshalapugin <dimasalapugin@gmail.com>
2023-05-19 12:24:51 +0300

    Done

--- string_challenges.py
+++ string_challenges.py
@@ -6,12 +6,13 @@ print(word[-1])
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-print(len([j for j in ''.join(word) if j.lower() == 'а']))
+print(word.lower().count('а'))
 
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-print(len([j for j in ''.join(word) if j.lower() not in ['а', 'е', 'у', 'ы', 'о', 'э', 'я', 'и', 'ю']]))
+vowels = ['а', 'е', 'у', 'ы', 'о', 'э', 'я', 'и', 'ю']
+print(len([letter for letter in word if letter.lower() not in vowels]))
 
 
 # Вывести количество слов в предложении
@@ -21,13 +22,13 @@ print(len(sentence.split()))
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-for i in ''.join(sentence).split():
-    print(i[0])
+for word in sentence.split():
+    print(word[0])
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-q = []
-for i in sentence.split():
-    q.append(len(i))
-print(int(sum(q)/len(sentence.split())))
+spisok = []
+for letter in sentence.split():
+    spisok.append(len(letter))
+print(int(sum(spisok)/len(sentence.split())))
